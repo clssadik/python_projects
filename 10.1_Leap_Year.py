1eccb9f975a399c781ffe98ce4e6d0e6eea1b0c0
clssadik <cillsadik@gmail.com>
2026-03-04 17:08:26 +0300

    leap year

---
+++ 10.1_Leap_Year.py
@@ -0,0 +1,11 @@
+def is_leap_year(year):
+    # Write your code here.
+    # Don't change the function name.
+    if year % 4 == 0 and year % 100 != 0 or year % 400 == 0
+        return True
+    else:
+        return False
+
+
+leap_number = is_leap_year(2024)
+print(leap_number)
