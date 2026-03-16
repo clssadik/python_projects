a4fc4dacac1b34d7a6454bebacdf8c6fe69ec325
Sadık Çil <cillsadik@gmail.com>
2026-03-16 14:08:44 +0300

    pretty tables

--- PythonOOP.py
+++ PythonOOP.py
@@ -1,12 +1,21 @@
-from turtle import Turtle, Screen
+#
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("orange")
+# timmy.forward(100)
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("orange")
-timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name",["Pikachu","Squirtle","Charmender"],"c")
+table.add_column("Type",["Type","Electric","Fire"],"c")
+print(table)
