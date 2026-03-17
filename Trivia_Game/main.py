732c4b23d66b73bb684edfb7c365429d417c2031
Sadık Çil <cillsadik@gmail.com>
2026-03-17 15:08:01 +0300

    trivia game created

--- Trivia_Game/main.py
+++ Trivia_Game/main.py
@@ -1,14 +1,17 @@
-from data import question_data
+from data import question_data,another_data
 from question_model import Question
 from quiz_brain import QuizBrain
 
 question_bank = []
-for letter in question_data:
-    question_text = letter["text"]
-    question_answer = letter["answer"]
+for letter in another_data:
+    question_text = letter["question"]
+    question_answer = letter["correct_answer"]
     question = Question(question_text, question_answer)
     question_bank.append(question)
 
 quiz = QuizBrain(question_bank)
 while quiz.still_has_question():
     quiz.next_question()
+
+print("You've completed the quiz!")
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
