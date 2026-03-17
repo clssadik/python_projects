ebf124a13c7cda04edc4330b9ca85cf9ce027ab3
Sadık Çil <cillsadik@gmail.com>
2026-03-17 14:25:10 +0300

    more classes

--- quiz-game-start/main.py
+++ quiz-game-start/main.py
@@ -1,5 +1,6 @@
 from data import question_data
 from question_model import Question
+from quiz_brain import QuizBrain
 
 question_bank = []
 for letter in question_data:
@@ -7,3 +8,7 @@ for letter in question_data:
     question_answer = letter["answer"]
     question = Question(question_text, question_answer)
     question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+while quiz.still_has_question():
+    quiz.next_question()
---
+++ quiz-game-start/quiz_brain.py
@@ -0,0 +1,15 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def still_has_question(self):
+        if self.question_number < 11:
+            return False
+        else:
+            return True
+
+    def next_question(self):
+        self.question_number += 1
+        current_question = self.question_list[self.question_number-1]
+        answer = input(f"Q.{self.question_number}: {current_question.text}. (True/False)?: ")
