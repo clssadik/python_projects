15e9a4c5e22372c493da31eeb6eb1fc45e53655a
Sadık Çil <cillsadik@gmail.com>
2026-03-18 15:43:22 +0300

    penup pendown

--- Turtle/main.py
+++ Turtle/main.py
@@ -1,3 +1,18 @@
+# from turtle import Turtle,Screen
+#
+# turtle = Turtle()
+# turtle.shape("arrow")
+# turtle.color("orange")
+# turtle.speed(1)
+#
+# for i in range(4):
+#     turtle.forward(100)
+#     turtle.left(90)
+#
+#
+# screen = Screen()
+# screen.exitonclick()
+
 from turtle import Turtle,Screen
 
 turtle = Turtle()
@@ -5,10 +20,11 @@ turtle.shape("arrow")
 turtle.color("orange")
 turtle.speed(1)
 
-for i in range(4):
-    turtle.forward(100)
-    turtle.left(90)
-
+for i in range(20):
+    turtle.forward(10)
+    turtle.penup()
+    turtle.forward(10)
+    turtle.pendown()
 
 screen = Screen()
 screen.exitonclick()
