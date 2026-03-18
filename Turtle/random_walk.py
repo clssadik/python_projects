b02a3926bc47462050d335d37d3574ef4647865d
Sadık Çil <cillsadik@gmail.com>
2026-03-18 16:46:00 +0300

    random_walk

---
+++ Turtle/random_walk.py
@@ -0,0 +1,28 @@
+from turtle import Turtle,Screen
+import random
+turtle = Turtle()
+
+colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepSkyBlue", "LightSeaGreen", "wheat", "SlateGray", "SeaGreen","red","blue","orange"]
+
+which_way = ["east","north","south","west"]
+
+
+turtle.shape("arrow")
+turtle.pensize(10)
+
+for i in range(100):
+    turtle.forward(30)
+    choice = random.choice(which_way)
+    if choice == "east":
+        turtle.setheading(0)
+    elif choice == "north":
+        turtle.setheading(90)
+    elif choice == "south":
+        turtle.setheading(270)
+    elif choice == "west":
+        turtle.setheading(180)
+    turtle.color(random.choice(colours))
+
+
+screen = Screen()
+screen.exitonclick()
