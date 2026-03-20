38d0ff42bfb9f62b15e8ab22cd7db9bafd77bc38
Sadık Çil <cillsadik@gmail.com>
2026-03-20 14:23:53 +0300

    wasd

--- HighOrderFunctions/turtle_example.py
+++ HighOrderFunctions/turtle_example.py
@@ -1,3 +1,16 @@
+# from turtle import Turtle,Screen
+#
+# turtle=Turtle()
+# screen=Screen()
+#
+# def move_forwards():
+#     turtle.forward(10)
+#
+# screen.listen()
+# screen.onkey(key="space", fun=move_forwards)
+# screen.exitonclick()
+#
+
 from turtle import Turtle,Screen
 
 turtle=Turtle()
@@ -5,8 +18,16 @@ screen=Screen()
 
 def move_forwards():
     turtle.forward(10)
+def move_backwards():
+    turtle.backward(10)
+def clockwise():
+    turtle.right(90)
+def counterclockwise():
+    turtle.left(90)
 
 screen.listen()
-screen.onkey(key="space", fun=move_forwards)
+screen.onkey(move_forwards,"w")
+screen.onkey(move_backwards,"s")
+screen.onkey(counterclockwise,"a")
+screen.onkey(clockwise,"d")
 screen.exitonclick()
-
