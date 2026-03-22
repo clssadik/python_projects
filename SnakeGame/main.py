c453d042c0a386094192c638f24a27feb8d90e6e
Sadık Çil <cillsadik@gmail.com>
2026-03-22 21:30:33 +0300

    snake game v2

--- SnakeGame/main.py
+++ SnakeGame/main.py
@@ -1,6 +1,7 @@
 import time
 from turtle import Turtle,Screen
-
+from scoreboard import Scoreboard
+from food import Food
 from snake import Snake
 
 screen = Screen()
@@ -10,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up,"Up")
@@ -22,6 +25,24 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        snake.extend()
+        scoreboard.increase_score()
+
+    #Detect collision with wall
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() < -280 or snake.head.ycor() > 280:
+        game_is_on = False
+        scoreboard.game_over()
+
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            game_is_on = False
+            scoreboard.game_over()
 
 
 
