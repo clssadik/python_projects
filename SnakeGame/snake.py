f5d21e462abe0eb397dbb5a812ba0c562a8369fb
Sadık Çil <cillsadik@gmail.com>
2026-03-22 18:51:24 +0300

    snake game v1

--- SnakeGame/snake.py
+++ SnakeGame/snake.py
@@ -1,5 +1,9 @@
 from turtle import Turtle
 START_POSITION = [(0, 0), (-20, 0), (-40, 0)]
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
 
 class Snake:
     def __init__(self):
@@ -23,10 +27,14 @@ class Snake:
         self.segments[0].forward(20)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
