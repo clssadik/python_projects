09960b3af51393ff4fc0242af0d175cf833aee09
Sadık Çil <cillsadik@gmail.com>
2026-04-03 21:19:20 +0300

    Constructor created

--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -1,4 +1,5 @@
 class Dog:
+    # Constructor - Bu nesne yaratılıcaksa bu şartlara uymak zorunda
     def __init__(self,name,breed):
         self.name = name
         self.breed = breed
@@ -6,5 +7,7 @@ class Dog:
     def bark(self):
         print("Woof")
 
-dog1 = Dog()
+dog1 = Dog("bruce","scottish")
+print(dog1.name)
+print(dog1.breed)
 dog1.bark()
