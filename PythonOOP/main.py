7eaa118ab121ef2a6d34aeecaabcf6c6b6780f08
Sadık Çil <cillsadik@gmail.com>
2026-04-03 21:26:06 +0300

    combining objects

--- PythonOOP/main.py
+++ PythonOOP/main.py
@@ -1,13 +1,22 @@
 class Dog:
     # Constructor - Bu nesne yaratılıcaksa bu şartlara uymak zorunda
-    def __init__(self,name,breed):
+    def __init__(self,name,breed,owner):
         self.name = name
         self.breed = breed
+        self.owner = owner
 
     def bark(self):
         print("Woof")
 
-dog1 = Dog("bruce","scottish")
+class Owner:
+    def __init__(self,name,address, number):
+        self.name = name
+        self.address = address
+        self.number = number
+
+owner1 = Owner("Sadik","Mersin","+9085")
+dog1 = Dog("bruce","scottish",owner1)
 print(dog1.name)
 print(dog1.breed)
+print(dog1.owner.name)
 dog1.bark()
