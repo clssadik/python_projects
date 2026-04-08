8878303ea1c31f8844800dd1d91c3c9d47e63b6e
Sadık Çil <cillsadik@gmail.com>
2026-04-08 20:58:30 +0300

    private attribute

--- PythonOOP/User.py
+++ PythonOOP/User.py
@@ -1,18 +1,21 @@
 class User:
     def __init__(self,username,email,password):
         self.username = username
-        self.email = email
+        self._email = email
         self.password = password
+
+    def clean_email(self):
+        return self._email.lower().strip()
 
     def say_hi_to_user(self,user):
         print(f"Sending message to {user.username}: Hi {user.username}, it's {self.username}")
 
-my_username = User("clssadik","cillsadik@gmail.com",0000)
+my_username = User("clssadik","CilLSaDik@gmail.com",0000)
 new_user = User("alice","alice@gmail.com",1111)
 
 my_username.say_hi_to_user(new_user)
 
-print(my_username.email)
+print(my_username.clean_email())
 
 # this is not good
 # make attribute private
