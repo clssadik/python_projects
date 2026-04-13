6e5d948309fdf90c4aa5cff0d64e6784e94c2877
Sadık Çil <cillsadik@gmail.com>
2026-04-13 12:46:34 +0300

    with function

--- open_file/main.py
+++ open_file/main.py
@@ -1,3 +1,8 @@
-file = open(file="my_file.txt")
-contents = file.read()
-print(contents)
+# file = open(file="my_file.txt")
+# contents = file.read()
+# print(contents)
+# file.close()
+
+with open("my_file.txt", "w") as file:
+    contents = file.read()
+    print(contents)
