4de1d456b6e31b5ce00d0de4a40eb2e3b59e2fcc
Sadık Çil <cillsadik@gmail.com>
2026-04-13 12:54:18 +0300

    new file created with function

--- open_file/main.py
+++ open_file/main.py
@@ -3,6 +3,5 @@
 # print(contents)
 # file.close()
 
-with open("my_file.txt", "w") as file:
-    contents = file.read()
-    print(contents)
+with open("new_file.txt",mode="w") as file:
+    file.write("hello world")
