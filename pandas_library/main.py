0202a9febd15116861714a9a963743f548e87758
Sadık Çil <cillsadik@gmail.com>
2026-04-14 10:47:10 +0300

    csv files

---
+++ pandas_library/main.py
@@ -0,0 +1,17 @@
+# with open("weather_data.csv") as data_file:
+#     data = data_file.readlines()
+#     print(data)
+
+import csv
+
+with open("weather_data.csv") as data_file:
+    data = csv.reader(data_file)
+    # print(data)
+    # for row in data:
+    #     print(row)
+
+    temperatures = []
+    next(data)
+    for row in data:
+        temperatures.append(int(row[1]))
+    print(temperatures)
