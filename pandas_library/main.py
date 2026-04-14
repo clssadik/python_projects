b2315a49d2000ff48b05eff0f7e12a3166f410d3
Sadık Çil <cillsadik@gmail.com>
2026-04-14 14:08:11 +0300

    data dict, list

--- pandas_library/main.py
+++ pandas_library/main.py
@@ -17,6 +17,14 @@
 #     print(temperatures)
 
 import pandas
+from numpy.ma.extras import average
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+
+# print(data["temp"])
+
+data_dict = data.to_dict()
+print(data_dict)
+data_list = data["temp"].to_list()
+print(data_list)
+print(average(data_list))
