b06a3cad178af446e4282ca6bcf450a0b3d3b4fd
Sadık Çil <cillsadik@gmail.com>
2026-04-14 11:07:57 +0300

    pandas library added

--- pandas_library/main.py
+++ pandas_library/main.py
@@ -2,16 +2,21 @@
 #     data = data_file.readlines()
 #     print(data)
 
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     # print(data)
+#     # for row in data:
+#     #     print(row)
+#
+#     temperatures = []
+#     next(data)
+#     for row in data:
+#         temperatures.append(int(row[1]))
+#     print(temperatures)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    # print(data)
-    # for row in data:
-    #     print(row)
+import pandas
 
-    temperatures = []
-    next(data)
-    for row in data:
-        temperatures.append(int(row[1]))
-    print(temperatures)
+data = pandas.read_csv("weather_data.csv")
+print(data["temp"])
