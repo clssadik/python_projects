6fbfbddf838ecb795f6dc20b24473e9e7acdc541
Sadık Çil <cillsadik@gmail.com>
2026-04-15 15:24:24 +0300

    first data analysis

--- pandas_library/main.py
+++ pandas_library/main.py
@@ -1,53 +1,90 @@
-# with open("weather_data.csv") as data_file:
-#     data = data_file.readlines()
-#     print(data)
-
-# import csv
+# # with open("weather_data.csv") as data_file:
+# #     data = data_file.readlines()
+# #     print(data)
 #
-# with open("weather_data.csv") as data_file:
-#     data = csv.reader(data_file)
-#     # print(data)
-#     # for row in data:
-#     #     print(row)
+# # import csv
+# #
+# # with open("weather_data.csv") as data_file:
+# #     data = csv.reader(data_file)
+# #     # print(data)
+# #     # for row in data:
+# #     #     print(row)
+# #
+# #     temperatures = []
+# #     next(data)
+# #     for row in data:
+# #         temperatures.append(int(row[1]))
+# #     print(temperatures)
 #
-#     temperatures = []
-#     next(data)
-#     for row in data:
-#         temperatures.append(int(row[1]))
-#     print(temperatures)
+# import pandas
+# from numpy.ma.extras import average
+#
+# data = pandas.read_csv("weather_data.csv")
+# #
+# # # print(data["temp"])
+# #
+# # # data_dict = data.to_dict()
+# # # print(data_dict)
+# # data_list = data["temp"].to_list()
+# #
+# # print(average(data_list))
+# # print(data["temp"].max())
+# # print(data.temp.max())
+#
+# # Get row from data table
+# print(data[data.day == "Monday"])
+# # Get max temp row
+# print(data[data.temp == data.temp.max()])
+#
+# monday = data[data.day == "Monday"]
+# fahr = (monday.temp[0] * 9 / 5) + 32
+# print(fahr)
+#
+# # Create dataframe from scratch
+# data_dict = {
+#     "students" : ["Amy","James", "Angela"],
+#     "scores" : [76, 56, 65]
+# }
+# data2 = pandas.DataFrame(data_dict)
+# data.to_csv("new_data2.csv")
+# print(data2)
 
 import pandas
-from numpy.ma.extras import average
 
-data = pandas.read_csv("weather_data.csv")
-#
-# # print(data["temp"])
-#
-# # data_dict = data.to_dict()
-# # print(data_dict)
-# data_list = data["temp"].to_list()
-#
-# print(average(data_list))
-# print(data["temp"].max())
-# print(data.temp.max())
+data = pandas.read_csv("abc.csv")
 
-# Get row from data table
-print(data[data.day == "Monday"])
-# Get max temp row
-print(data[data.temp == data.temp.max()])
+gray = len(data[data["Primary Fur Color"] == "Gray"])
+red = len(data[data["Primary Fur Color"] == "Cinnamon"])
+black = len(data[data["Primary Fur Color"] == "Black"])
 
-monday = data[data.day == "Monday"]
-fahr = (monday.temp[0] * 9 / 5) + 32
-print(fahr)
-
-# Create dataframe from scratch
 data_dict = {
-    "students" : ["Amy","James", "Angela"],
-    "scores" : [76, 56, 65]
+    "Fur Color" : ["gray", "red", "black"],
+    "Count" : [gray, red, black]
 }
+
 data2 = pandas.DataFrame(data_dict)
-data.to_csv("new_data2.csv")
 print(data2)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
