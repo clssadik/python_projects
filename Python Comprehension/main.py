6bb6acbe3f8ee226bf2ded6d1cbdc215dde6b752
Sadık Çil <cillsadik@gmail.com>
2026-04-17 12:58:40 +0300

    python exercise

--- Python Comprehension/main.py
+++ Python Comprehension/main.py
@@ -16,3 +16,16 @@ list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
 numbers = [int(num) for num in list_of_strings]
 result = [num for num in numbers if num%2 == 0]
 print(result)
+
+with open("file1.txt") as d1:
+    list1 = d1.read().splitlines()
+
+with open("file2.txt") as d2:
+    list2 = d2.read().splitlines()
+
+result = [int(num) for num in list1 if num in list2]
+
+print(result)
+
+# .readlines() -> Çıktısı: ['1\n', '2\n', '3']
+# .splitlines() -> Çıktısı: ['1', '2', '3']
