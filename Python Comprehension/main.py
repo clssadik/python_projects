6866aed0ff39c1eab083522ac558355b92b1b508
Sadık Çil <cillsadik@gmail.com>
2026-04-17 12:38:28 +0300

    new exercise

--- Python Comprehension/main.py
+++ Python Comprehension/main.py
@@ -8,3 +8,11 @@ print(new_range)
 names = ["Alex","Beth","Caroline","Dave","Elanor","Freddie"]
 short_names = [name for name in names if len(name)<5]
 print(short_names)
+
+upper_case_names = [name.upper() for name in names if len(name)>5]
+print(upper_case_names)
+
+list_of_strings = ['9', '0', '32', '8', '2', '8', '64', '29', '42', '99']
+numbers = [int(num) for num in list_of_strings]
+result = [num for num in numbers if num%2 == 0]
+print(result)
