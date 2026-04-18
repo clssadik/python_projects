0d8f2ab2e85337de22e799bcc8f0dad1df917e5f
Sadık Çil <cillsadik@gmail.com>
2026-04-18 13:42:32 +0300

    dictionaries comprehension

---
+++ Python Comprehension/dict_comprehension.py
@@ -0,0 +1,15 @@
+# new_dict = {new_key:new_value for item in list}
+# new_dict = {new_key:new_value for (key,value) in dict.items()
+
+names = ["Alex","Beth","Caroline","Dave","Elanor","Freddie"]
+import random
+students_score = {student:random.randint(1,100) for student in names}
+print(students_score)
+passed_students = {student:score for (student,score) in students_score.items() if score >= 60}
+print(passed_students)
+
+#
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+sentence_list = sentence.split()
+result = {sent:len(sent) for sent in sentence_list}
+print(result)
