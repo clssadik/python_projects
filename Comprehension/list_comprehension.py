65e9c249c636df88316befd0d20bee81a8c86747
Sadık Çil <cillsadik@gmail.com>
2026-04-20 18:45:14 +0300

    list comprehension completed

--- Comprehension/list_comprehension.py
+++ Comprehension/list_comprehension.py
@@ -1,3 +1,13 @@
 # List comprehension
-# new_list = [new_item for item in list]
+# new_list = [n for n in list]
 
+numbers = [1,2,3]
+new_numbers = [n+1 for n in numbers]
+
+new_list = [n*n for n in range(1,5)]
+print(new_list)
+
+names = ["Alex","Beth","Caroline","Elanor","Freddie"]
+short_names = [name for name in names if len(name) < 5]
+long_names = [name.upper() for name in names if len(name) >= 5]
+print(long_names)
