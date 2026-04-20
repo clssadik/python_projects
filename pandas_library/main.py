ba92c3a22cf665df4305639d1f6867471a6f476d
Sadık Çil <cillsadik@gmail.com>
2026-04-20 17:59:48 +0300

    avg temp

--- pandas_library/main.py
+++ pandas_library/main.py
@@ -1,4 +1,11 @@
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
+print(type(data.day))
 print(data)
+
+data_dict = data.to_dict()
+
+temp_list = data.temp.tolist()
+# average temparature
+print(sum(temp_list) / len(temp_list) )
