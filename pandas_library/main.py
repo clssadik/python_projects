4a00a13253f243883a85f02b7c71e029e6b98dab
Sadık Çil <cillsadik@gmail.com>
2026-04-20 18:02:45 +0300

    get data in row

--- pandas_library/main.py
+++ pandas_library/main.py
@@ -7,5 +7,9 @@ print(data)
 data_dict = data.to_dict()
 
 temp_list = data.temp.tolist()
+
 # average temparature
 print(sum(temp_list) / len(temp_list) )
+
+# get data in rows ***
+print(data[data.temp == data.temp.max()])
