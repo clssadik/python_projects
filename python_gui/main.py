26c28565486cf150327d471774653e191689f3de
Sadık Çil <cillsadik@gmail.com>
2026-04-22 19:31:19 +0300

    update

--- python_gui/main.py
+++ python_gui/main.py
@@ -4,6 +4,8 @@ window = tkinter.Tk()
 window.title("Python GUI")
 window.minsize(500, 300)
 
+my_label = tkinter.Label(text="Hello World", font=("Times", 25, "italic"))
+my_label.pack(expand=True)
 
 
 
