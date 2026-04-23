f4d3cfa95084ad5ef4223dfcc9d5fe0439306049
clssadik <cillsadik@gmail.com>
2026-04-23 20:15:27 +0300

    new features

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,5 +1,6 @@
 from tkinter import PhotoImage
 from tkinter import *
+import math
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,17 +16,21 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
 def start_timer():
-    count_down(5)
-
+    count_down(5*60)
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
 def count_down(count):
-    canvas.itemconfig(timer_text, text = count)
+    count_min = math.floor(count / 60)
+    count_sec = count % 60
+
+
+    canvas.itemconfig(timer_text, text = f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000,count_down,count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
+
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50,bg=YELLOW)
