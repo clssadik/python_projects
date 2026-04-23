384a0edeb74c5819bd3eaf5223329b2c1f8acad9
clssadik <cillsadik@gmail.com>
2026-04-23 19:07:15 +0300

    added some feature

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -19,11 +19,26 @@ LONG_BREAK_MIN = 20
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
+window.config(padx=100, pady=50,bg=YELLOW)
 
-canvas = Canvas(width=200, height=224)
+timer = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,40, "bold"))
+timer.grid(row=0,column=1)
+
+start_button = Button(text="Start",bg=YELLOW)
+start_button.grid(row=2,column=0)
+
+reset_button = Button(text="Reset",bg=YELLOW)
+reset_button.grid(row=2,column=2)
+
+tick_label = Label(text="✔",bg=YELLOW,fg=GREEN,font=(30))
+tick_label.grid(row=3,column=1)
+
+canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.pack()
+canvas.create_text(100,130, text="00:00",fill="white",font=(FONT_NAME,35, "bold"))
+canvas.grid(row=1,column=1)
+
 
 
 
