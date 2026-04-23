8e52428e9c0aaee3c0cb1e983634f02196588b23
clssadik <cillsadik@gmail.com>
2026-04-23 20:08:57 +0300

    added start_timer

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -14,20 +14,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- TIMER MECHANISM ------------------------------- # 
 
+def start_timer():
+    count_down(5)
+
+
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
+
+def count_down(count):
+    canvas.itemconfig(timer_text, text = count)
+    if count > 0:
+        window.after(1000,count_down,count-1)
 
 # ---------------------------- UI SETUP ------------------------------- #
 window = Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50,bg=YELLOW)
 
+
 timer = Label(text="Timer",fg=GREEN,bg=YELLOW,font=(FONT_NAME,40, "bold"))
 timer.grid(row=0,column=1)
 
-start_button = Button(text="Start",bg=YELLOW)
+start_button = Button(text="Start",highlightthickness=0,highlightbackground=YELLOW,command=start_timer)
 start_button.grid(row=2,column=0)
 
-reset_button = Button(text="Reset",bg=YELLOW)
+reset_button = Button(text="Reset",highlightthickness=0,highlightbackground=YELLOW)
 reset_button.grid(row=2,column=2)
 
 tick_label = Label(text="✔",bg=YELLOW,fg=GREEN,font=(30))
@@ -36,7 +46,7 @@ tick_label.grid(row=3,column=1)
 canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
 tomato_img = PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
-canvas.create_text(100,130, text="00:00",fill="white",font=(FONT_NAME,35, "bold"))
+timer_text = canvas.create_text(100,130, text="00:00",fill="white",font=(FONT_NAME,35, "bold"))
 canvas.grid(row=1,column=1)
 
 
