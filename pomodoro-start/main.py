4029e3dce5d57afa6c2eb7cb07f1c1acb77c644a
clssadik <cillsadik@gmail.com>
2026-04-25 12:40:38 +0300

    add tick

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -7,7 +7,7 @@ RED = "#e7305b"
 GREEN = "#9bdeac"
 YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
-WORK_MIN = 1
+WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 reps = 0
@@ -49,6 +49,8 @@ def count_down(count):
         window.after(1000,count_down,count-1)
     else:
         start_timer()
+        if reps%2 == 0:
+            tick_label.config(text="✔" * int(reps / 2))
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -66,7 +68,7 @@ start_button.grid(row=2,column=0)
 reset_button = Button(text="Reset",highlightthickness=0,highlightbackground=YELLOW)
 reset_button.grid(row=2,column=2)
 
-tick_label = Label(text="✔",bg=YELLOW,fg=GREEN,font=(30))
+tick_label = Label(bg=YELLOW,fg=GREEN,font=(30))
 tick_label.grid(row=3,column=1)
 
 canvas = Canvas(width=200, height=224,bg=YELLOW,highlightthickness=0)
