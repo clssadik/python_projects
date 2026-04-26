b2324613416f890d75e1a746915487e097ef60cf
clssadik <cillsadik@gmail.com>
2026-04-26 19:07:11 +0300

    awesome password generator

--- password_manager/main.py
+++ password_manager/main.py
@@ -1,10 +1,33 @@
+import pyperclip
 from tkinter import Entry
 from tkinter import Label
 from tkinter import * 
+from tkinter import messagebox
+import random
 
 
 
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+def password_generator():
+    letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+    numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+    # new_list = [n for n in list]
+
+    password_letter = [random.choice(letters) for _ in range(random.randint(8,10))]
+    password_number = [random.choice(numbers) for _ in range(random.randint(2,4))]
+    password_symbol = [random.choice(symbols) for _ in range(random.randint(2,4))]
+
+    password_list = password_letter + password_number + password_symbol
+    random.shuffle(password_list)
+
+    password = "".join(password_list)
+
+    password_data.delete(0,END)
+    password_data.insert(0,password)
+    pyperclip.copy(password)
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
@@ -12,9 +35,19 @@ def save_data():
     website = website_data.get()
     email = email_username_data.get()
     password = password_data.get()
+
+    if len(website) == 0 or len(password) == 0 or len(email) == 0:
+        messagebox.showwarning(title="Warning",message="Please fill in the required fields!")
+
+    else:  
+        is_okay = messagebox.askokcancel(title=website,message=f"Email : {email}\nPassword : {password}\nWould you like to save it?")
+        if is_okay:
+            with open("password_manager.txt","a") as file:
+                file.write(f"{website} | {email} | {password}\n")
+
+    website_data.delete(0,END)
+    password_data.delete(0,END)
     
-    with open("password_manager.txt","a") as file:
-        file.write(f"{website} | {email} | {password}\n")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -47,7 +80,7 @@ email_username_data.grid(row=2,column=1,columnspan=2)
 password_data = Entry(width=20,background="white",fg="black")
 password_data.grid(row=3,column=1)
 
-generate = Button(text="Generate Password",background="white",width=11)
+generate = Button(text="Generate Password",background="white",width=11,command=password_generator)
 generate.grid(row=3,column=2)
 
 add = Button(width=33, text="Add",command=save_data)
