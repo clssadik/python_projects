6228bb35d15c1bb8c5eae88ebfe3d3449e9d60c0
clssadik <cillsadik@gmail.com>
2026-04-26 18:01:41 +0300

    button clicked

--- password_manager/main.py
+++ password_manager/main.py
@@ -7,12 +7,14 @@ from tkinter import *
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
+def button_clicked():
+    print("blabla")
 
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
 window.title("Password Manager")
-window.config(padx=20, pady=20)
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200, highlightthickness=0)
 photo = PhotoImage(file="logo.png")
@@ -30,8 +32,10 @@ password_label.grid(row=3,column=0)
 
 website_data = Entry(width=35,background="white",fg="black")
 website_data.grid(row=1,column=1,columnspan=2)
+website_data.focus()
 
 email_username_data = Entry(width=35,background="white",fg="black")
+email_username_data.insert(0,"cillsadik@gmail.com")
 email_username_data.grid(row=2,column=1,columnspan=2)
 
 password_data = Entry(width=20,background="white",fg="black")
@@ -40,7 +44,7 @@ password_data.grid(row=3,column=1)
 generate = Button(text="Generate Password",background="white",width=11)
 generate.grid(row=3,column=2)
 
-add = Button(width=33, text="Add")
+add = Button(width=33, text="Add",command=button_clicked)
 add.grid(row=4,column=1,columnspan=2)
 
 
