81ac749dccbd0ad0217e95bfd71ff0f15eacf607
clssadik <cillsadik@gmail.com>
2026-04-27 20:05:13 +0300

    final state

--- json_file/main.py
+++ json_file/main.py
@@ -3,7 +3,10 @@
 try:
     file = open("a_file.txt")
     a_dictionary = {"key": "value"}
+    # Different type of error
     print(a_dictionary["fmodjfjns"])
 except FileNotFoundError:
     file = open("a_file.txt", "w")
     file.write("Something")
+except KeyError as error_message:
+    print(f"That key {error_message} does not exist")
