22b09c90dd530a892271b47cd3ea47771ec16b2a
clssadik <cillsadik@gmail.com>
2026-04-28 21:44:00 +0300

    try catch completed

--- try_catch/main.py
+++ try_catch/main.py
@@ -1,6 +1,21 @@
 #File not found
+import os
+os.chdir(os.path.dirname(__file__)) 
 
 try:
     file = open("a_file.txt")
-except:
+    my_dict = {"key": "value"}
+    # print(my_dict["msdfmjsdfj"])
+    print(my_dict["key"])
+except FileNotFoundError:
     file  = open("a_file.txt", "w")
+    file.write("Something")
+except KeyError as error_msg:
+    print(f"key does not exist {error_msg}")
+else:
+    content = file.read()
+    print(content)
+finally:
+    file.close()
+    # print("File was closed.")
+    raise KeyError
