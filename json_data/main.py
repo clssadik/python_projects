933529d2c038961edaf49e8dd2bb65aa1442d5e5
clssadik <cillsadik@gmail.com>
2026-05-03 13:52:38 +0300

    new exercises

--- json_data/main.py
+++ json_data/main.py
@@ -31,4 +31,39 @@ if height > 3:
 bmi = weight / height ** 2
 print(bmi)
 
+fruits = ["Apple", "Pear", "Orange"]
 
+# Catch the exception and make sure the code runs without crashing.
+def make_pie(index):
+    fruit = fruits[index]
+    print(fruit + " pie")
+
+try:
+    make_pie(4)
+except IndexError:
+    print("Fruit pie")
+
+
+facebook_posts = [
+    {'Likes': 21, 'Comments': 2},
+    {'Likes': 13, 'Comments': 2, 'Shares': 1},
+    {'Likes': 33, 'Comments': 8, 'Shares': 3},
+    {'Comments': 4, 'Shares': 2},
+    {'Comments': 1, 'Shares': 1},
+    {'Likes': 19, 'Comments': 3}
+]
+
+
+def count_likes(posts):
+
+    total_likes = 0
+    for post in posts:
+        try:
+            total_likes = total_likes + post['Likes']
+        except:
+            pass
+    
+    return total_likes
+
+
+count_likes(facebook_posts)
