6cb011ae4a8a3ae703904ab0ae9ea7032982cb27
clssadik <cillsadik@gmail.com>
2026-05-03 13:15:56 +0300

    try_except_else blocks

--- json_data/main.py
+++ json_data/main.py
@@ -2,6 +2,18 @@
 
 try:
     file = open("a_file.txt")
-except:
+    a_dictionary = {"key":"value"}
+    # print(a_dictionary["dfdfıjkısd"])
+    print(a_dictionary["key"])
+
+
+# I should never use a bare except block without specifying an exception type.
+except FileNotFoundError:
     file = open("a_file.txt","w")
     file.write("Something")
+except KeyError as error_message:
+    print(f"The key {error_message} does not exist")
+# try bloğundan sonra eğer except içerisine girmediyse else triggerlanır
+else:
+    content = file.read()
+    print(content)
