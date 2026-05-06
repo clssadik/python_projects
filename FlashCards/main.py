00c0fac0619f1bd02428d6e873ffaf98179fa2e9
clssadik <cillsadik@gmail.com>
2026-05-06 16:57:50 +0300

    json file added

--- FlashCards/main.py
+++ FlashCards/main.py
@@ -1,6 +1,8 @@
+from gc import disable
 from tkinter import *
 import pandas
 import random
+import json
 
 BACKGROUND_COLOR = "#B1DDC6"
 
@@ -8,13 +10,29 @@ data_file = pandas.read_csv("data/french_words.csv")
 cards = data_file.to_dict(orient="records")
 current_card = random.choice(cards)
 
-def reset_front():
+def reverse_card():
     global current_card
     current_card = random.choice(cards)
     canvas.itemconfig(title_id,text="French",fill="black")
     canvas.itemconfig(word_id, text=current_card["French"], fill="black")
     canvas.itemconfig(canvas_image,image=front)
     window.after(3000,flip_card)
+
+def right_clicked():
+    try:
+        with open("data/known_words.json","r") as kelime:
+            data = json.load(kelime)
+            data.append(current_card)
+        
+    except FileNotFoundError:
+        with open("data/known_words.json","w") as kelime:
+            json.dump([current_card],kelime,indent=4)
+
+    else:
+        with open("data/known_words.json","w") as kelime:
+            json.dump(data,kelime,indent=4)
+    
+    reverse_card()
 
 def flip_card():
     canvas.itemconfig(word_id, text=current_card["English"], fill="white")
@@ -37,12 +55,15 @@ title_id = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "itali
 word_id = canvas.create_text(400, 263, text=current_card["French"], font=("Ariel", 60, "bold"), fill="black")
 
 wrong_image = PhotoImage(file="images/wrong.png")
-button1 = Button(image=wrong_image, highlightthickness=0,borderwidth=0,relief="flat",command=reset_front)
+button1 = Button(image=wrong_image, highlightthickness=0,borderwidth=0,relief="flat",command=reverse_card)
 button1.grid(row=1,column=0)
 
 right_image = PhotoImage(file="images/right.png")
-button2 = Button(image=right_image,highlightthickness=0,borderwidth=0,relief="flat",command=reset_front)
+button2 = Button(image=right_image,highlightthickness=0,borderwidth=0,relief="flat",command=right_clicked)
 button2.grid(row=1,column=1)
+
+button3 = Button(text="Known Words",highlightthickness=0,background=BACKGROUND_COLOR,width=20,borderwidth=0,relief="flat",activebackground=BACKGROUND_COLOR,highlightbackground=BACKGROUND_COLOR)
+button3.grid(row=2,column=0,columnspan=2)
 
 window.mainloop()
 
