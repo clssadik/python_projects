5f508beeac1058ce21b5a1032bb001244389b200
clssadik <cillsadik@gmail.com>
2026-05-08 14:33:35 +0300

    birthday date

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -16,3 +16,10 @@
 import datetime as dt
 
 now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(day_of_week)
+
+date_of_birth = dt.datetime(year=2002,month=9,day=27)
+print()
