4e73e6add13ebd07bf3985824a3f04a9a7553a8f
clssadik <cillsadik@gmail.com>
2026-05-08 14:58:58 +0300

    email sent

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -8,11 +8,14 @@ password = "lrktocahhgcsipks"
 now = dt.datetime.now()
 day_of_week = now.weekday()
 
-# with smtplib.SMTP("smtp.gmail.com",587) as connection:
-#     connection.starttls()
-#     connection.login(user=my_email,password=password)
-#     connection.sendmail(
-#         from_addr=my_email,
-#         to_addrs="22220030090@mersin.edu.tr",
-#         msg=f"Subject:Hello\n\nThis is the body of my email."
-#     )
+with open("quotes.txt") as file:
+    array = [satir.strip() for satir in file]
+
+with smtplib.SMTP("smtp.gmail.com",587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="22220030090@mersin.edu.tr",
+        msg=f"Subject:Hello\n\n{array[day_of_week]}".encode("utf-8")
+    )
