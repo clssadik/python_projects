421cfa5a9fd2d216f8e09d050877fa153a16c472
clssadik <cillsadik@gmail.com>
2026-05-08 13:36:34 +0300

    with functions added

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -4,12 +4,11 @@ import smtplib
 my_email = "cillsadik@gmail.com"
 password = "lrktocahhgcsipka"
 
-connection = smtplib.SMTP("smtp.gmail.com",587)
-connection.starttls()
-connection.login(user=my_email,password=password)
-connection.sendmail(
-    from_addr=my_email,
-    to_addrs="22220030090@mersin.edu.tr",
-    msg=f"Subject:Hello\n\nThis is the body of my email."
-)
-connection.close()
+with smtplib.SMTP("smtp.gmail.com",587) as connection:
+    connection.starttls()
+    connection.login(user=my_email,password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs="22220030090@mersin.edu.tr",
+        msg=f"Subject:Hello\n\nThis is the body of my email."
+    )
