80cb719be01df80c844db43c96f39f0edeaa3f06
clssadik <cillsadik@gmail.com>
2026-05-08 14:27:08 +0300

    datetime

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -1,14 +1,18 @@
-import smtplib
+# import smtplib
 
 
-my_email = "cillsadik@gmail.com"
-password = "lrktocahhgcsipka"
+# my_email = "cillsadik@gmail.com"
+# password = "lrktocahhgcsipka"
 
-with smtplib.SMTP("smtp.gmail.com",587) as connection:
-    connection.starttls()
-    connection.login(user=my_email,password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs="22220030090@mersin.edu.tr",
-        msg=f"Subject:Hello\n\nThis is the body of my email."
-    )
+# with smtplib.SMTP("smtp.gmail.com",587) as connection:
+#     connection.starttls()
+#     connection.login(user=my_email,password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs="22220030090@mersin.edu.tr",
+#         msg=f"Subject:Hello\n\nThis is the body of my email."
+#     )
+
+import datetime as dt
+
+now = dt.datetime.now()
