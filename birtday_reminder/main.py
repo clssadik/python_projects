bd8b6ff84f209796db562f038cb187f2fd7fd317
clssadik <cillsadik@gmail.com>
2026-05-08 14:47:51 +0300

    quotes added

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -1,8 +1,12 @@
-# import smtplib
+import smtplib
+import datetime as dt
 
 
-# my_email = "cillsadik@gmail.com"
-# password = "lrktocahhgcsipka"
+my_email = "cillsadik@gmail.com"
+password = "lrktocahhgcsipks"
+
+now = dt.datetime.now()
+day_of_week = now.weekday()
 
 # with smtplib.SMTP("smtp.gmail.com",587) as connection:
 #     connection.starttls()
@@ -12,14 +16,3 @@
 #         to_addrs="22220030090@mersin.edu.tr",
 #         msg=f"Subject:Hello\n\nThis is the body of my email."
 #     )
-
-import datetime as dt
-
-now = dt.datetime.now()
-year = now.year
-month = now.month
-day_of_week = now.weekday()
-print(day_of_week)
-
-date_of_birth = dt.datetime(year=2002,month=9,day=27)
-print()
