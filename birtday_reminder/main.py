b23e6e704ca0bf4ad04b33d231b81b3552feecdc
clssadik <cillsadik@gmail.com>
2026-05-08 13:34:13 +0300

    mailsended

--- birtday_reminder/main.py
+++ birtday_reminder/main.py
@@ -4,11 +4,11 @@ import smtplib
 my_email = "cillsadik@gmail.com"
 password = "lrktocahhgcsipks"
 
-connection = smtplib.SMTP("smtp@gmail.com")
+connection = smtplib.SMTP("smtp.gmail.com",587)
 connection.starttls()
 connection.login(user=my_email,password=password)
 connection.sendmail(
-    user=my_email,
+    from_addr=my_email,
     to_addrs="22220030090@mersin.edu.tr",
     msg=f"Subject:Hello\n\nThis is the body of my email."
 )
