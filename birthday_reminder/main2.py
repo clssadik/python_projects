1a37359a1d71563cbfffdfe971cce656191591b4
clssadik <cillsadik@gmail.com>
2026-05-09 12:49:08 +0300

    random method added

--- birthday_reminder/main2.py
+++ birthday_reminder/main2.py
@@ -10,14 +10,17 @@
 
 import datetime as dt
 import pandas
+import random
 
 now = dt.datetime.now()
 month = now.month
 day = now.day
+random_int = random.randint(1,3)
+print(random_int)
 
 data_file = pandas.read_csv("birthdays.csv")
 
-if month == data_file["month"][0] and day == ["day"][0]:
+# if month == data_file["month"][0] and day == ["day"][0]:
 
 
 print(day)
