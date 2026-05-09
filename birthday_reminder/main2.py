d8c9ef2dd9629b778ca33ac1d12b18ae9d14ba97
clssadik <cillsadik@gmail.com>
2026-05-09 12:43:50 +0300

    some changes added

---
+++ birthday_reminder/main2.py
@@ -0,0 +1,24 @@
+##################### Extra Hard Starting Project ######################
+
+# 1. Update the birthdays.csv
+
+# 2. Check if today matches a birthday in the birthdays.csv
+
+# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
+
+# 4. Send the letter generated in step 3 to that person's email address.
+
+import datetime as dt
+import pandas
+
+now = dt.datetime.now()
+month = now.month
+day = now.day
+
+data_file = pandas.read_csv("birthdays.csv")
+
+if month == data_file["month"][0] and day == ["day"][0]:
+
+
+print(day)
+
