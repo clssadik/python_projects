34d209b7dd05c25051c22a30db70dde91fc4d754
clssadik <cillsadik@gmail.com>
2026-05-10 13:43:13 +0300

    istanbul json files

--- api_endpoint/main.py
+++ api_endpoint/main.py
@@ -1,14 +1,28 @@
+# import requests
+
+# response = requests.get(url="http://api.open-notify.org/iss-now.json")
+# response.raise_for_status()
+
+# data = response.json()
+
+# longitude = data["iss_position"]["longitude"]
+# latitude = data["iss_position"]["latitude"]
+
+# iss_position = (longitude,latitude)
+
+# print(iss_position)
+
 import requests
 
-response = requests.get(url="http://api.open-notify.org/iss-now.json")
-response.raise_for_status()
+MY_LAT = 41.0707456
+MY_LONG = 29.0509414
+
+parameters = {
+    "lat" : MY_LAT,
+    "lng" : MY_LONG
+}
+
+response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
 
 data = response.json()
-
-longitude = data["iss_position"]["longitude"]
-latitude = data["iss_position"]["latitude"]
-
-iss_position = (longitude,latitude)
-
-print(iss_position)
-
+print(data)
