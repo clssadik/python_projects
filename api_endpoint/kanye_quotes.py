eff3fa0ae08cd2f0431b46a1b51deca834c113c5
clssadik <cillsadik@gmail.com>
2026-05-10 13:24:48 +0300

    completed

--- api_endpoint/kanye_quotes.py
+++ api_endpoint/kanye_quotes.py
@@ -3,7 +3,11 @@ import requests
 
 
 def get_quote():
+    
     response = requests.get("https://api.kanye.rest")
+    data = response.json()
+    quotes = data["quote"]
+    canvas.itemconfig(quote_text,text=quotes)
 
 
 
@@ -14,7 +18,7 @@ window.config(padx=50, pady=50)
 canvas = Canvas(width=300, height=414)
 background_img = PhotoImage(file="background.png")
 canvas.create_image(150, 207, image=background_img)
-quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 30, "bold"), fill="white")
+quote_text = canvas.create_text(150, 207, text="Kanye Quote Goes HERE", width=250, font=("Arial", 15, "bold"), fill="white")
 canvas.grid(row=0, column=0)
 
 kanye_img = PhotoImage(file="kanye.png")
