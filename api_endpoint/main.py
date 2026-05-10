4ecd8547a9c057f1b150bd8b58eba1b8d6acaea8
clssadik <cillsadik@gmail.com>
2026-05-10 14:07:28 +0300

    splitted

--- api_endpoint/main.py
+++ api_endpoint/main.py
@@ -13,16 +13,26 @@
 # print(iss_position)
 
 import requests
+from datetime import *
 
 MY_LAT = 41.0707456
 MY_LONG = 29.0509414
 
 parameters = {
     "lat" : MY_LAT,
-    "lng" : MY_LONG
+    "lng" : MY_LONG,
+    "formatted" : 0
 }
 
 response = requests.get("https://api.sunrise-sunset.org/json",params=parameters)
 
 data = response.json()
-print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+print(sunrise)
+print(sunset)
+
+now = datetime.now()
+
+print(now.hour)
