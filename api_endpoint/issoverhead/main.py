b62d2c7464fceeaf5e48cef676809fca6fbcf40c
clssadik <cillsadik@gmail.com>
2026-05-11 22:14:44 +0300

    program will run every 60 seconds

--- api_endpoint/issoverhead/main.py
+++ api_endpoint/issoverhead/main.py
@@ -9,47 +9,49 @@ MY_LONG = 29.0509414
 my_email = "cillsadik@gmail.com"
 password = "lrktocahhgcsipks"
 
-response_iss = requests.get(url="http://api.open-notify.org/iss-now.json")
-response_iss.raise_for_status()
-data = response_iss.json()
-
-iss_latitude = float(data["iss_position"]["latitude"])
-iss_longitude = float(data["iss_position"]["longitude"])
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
     "formatted": 0,
     "tzid": "Europe/Istanbul"
-
 }
 
-response_sun = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
-response_sun.raise_for_status()
-data_sun = response_sun.json()
-sunrise = int(data_sun["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data_sun["results"]["sunset"].split("T")[1].split(":")[0])
+while True:
 
-time_now = datetime.now()
+    response_iss = requests.get(url="http://api.open-notify.org/iss-now.json")
+    response_iss.raise_for_status()
+    data = response_iss.json()
 
-#If the ISS is close to my current position
-# and it is currently dark
-# Then send me an email to tell me to look up.
-# BONUS: run the code every 60 seconds.
+    iss_latitude = float(data["iss_position"]["latitude"])
+    iss_longitude = float(data["iss_position"]["longitude"])
 
-if iss_latitude-5 <= MY_LAT <= iss_latitude+5 and iss_longitude-5 <= MY_LONG <= iss_longitude+5:
-    
-    if sunset <= time_now.hour <= 24 or 0 <= time_now.hour <= sunrise:
+    response_sun = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
+    response_sun.raise_for_status()
+    data_sun = response_sun.json()
+    sunrise = int(data_sun["results"]["sunrise"].split("T")[1].split(":")[0])
+    sunset = int(data_sun["results"]["sunset"].split("T")[1].split(":")[0])
 
-        with open("example.txt") as file:
-            icerik = file.read()
-            
-            with smtplib.SMTP("smtp.gmail.com",587) as connection:
-                connection.starttls()
-                connection.login(user=my_email,password=password)
-                connection.sendmail(
-                    from_addr=my_email,
-                    to_addrs="22220030090@mersin.edu.tr"
-                    msg=f"Subject:Look Up!\n\n{icerik}".encode("utf-8")
-                )
+    time_now = datetime.now()
+
+    #If the ISS is close to my current position
+    # and it is currently dark
+    # Then send me an email to tell me to look up.
+    # BONUS: run the code every 60 seconds.
+
+    if iss_latitude-5 <= MY_LAT <= iss_latitude+5 and iss_longitude-5 <= MY_LONG <= iss_longitude+5:
+        
+        if sunset <= time_now.hour <= 24 or 0 <= time_now.hour <= sunrise:
+
+            with open("example.txt") as file:
+                icerik = file.read()
+                
+                with smtplib.SMTP("smtp.gmail.com",587) as connection:
+                    connection.starttls()
+                    connection.login(user=my_email,password=password)
+                    connection.sendmail(
+                        from_addr=my_email,
+                        to_addrs="22220030090@mersin.edu.tr"
+                        msg=f"Subject:Look Up!\n\n{icerik}".encode("utf-8")
+                    )
+    time.sleep(60)
 
