be9a930008c7d0c67cedc35743d6988e1e8ecfce
clssadik <cillsadik@gmail.com>
2026-05-11 22:13:10 +0300

    time added

--- api_endpoint/issoverhead/main.py
+++ api_endpoint/issoverhead/main.py
@@ -1,6 +1,7 @@
 import requests
 from datetime import datetime
 import smtplib
+import time
 
 MY_LAT = 41.0707456
 MY_LONG = 29.0509414
@@ -15,9 +16,6 @@ data = response_iss.json()
 iss_latitude = float(data["iss_position"]["latitude"])
 iss_longitude = float(data["iss_position"]["longitude"])
 
-#Your position is within +5 or -5 degrees of the ISS position.
-
-
 parameters = {
     "lat": MY_LAT,
     "lng": MY_LONG,
@@ -28,15 +26,11 @@ parameters = {
 
 response_sun = requests.get("https://api.sunrise-sunset.org/json", params=parameters)
 response_sun.raise_for_status()
-data = response_sun.json()
-sunrise = int(data["results"]["sunrise"].split("T")[1].split(":")[0])
-sunset = int(data["results"]["sunset"].split("T")[1].split(":")[0])
-
-print(sunrise," ",sunset)
+data_sun = response_sun.json()
+sunrise = int(data_sun["results"]["sunrise"].split("T")[1].split(":")[0])
+sunset = int(data_sun["results"]["sunset"].split("T")[1].split(":")[0])
 
 time_now = datetime.now()
-
-print(time_now.hour)
 
 #If the ISS is close to my current position
 # and it is currently dark
@@ -47,4 +41,15 @@ if iss_latitude-5 <= MY_LAT <= iss_latitude+5 and iss_longitude-5 <= MY_LONG <= 
     
     if sunset <= time_now.hour <= 24 or 0 <= time_now.hour <= sunrise:
 
+        with open("example.txt") as file:
+            icerik = file.read()
+            
+            with smtplib.SMTP("smtp.gmail.com",587) as connection:
+                connection.starttls()
+                connection.login(user=my_email,password=password)
+                connection.sendmail(
+                    from_addr=my_email,
+                    to_addrs="22220030090@mersin.edu.tr"
+                    msg=f"Subject:Look Up!\n\n{icerik}".encode("utf-8")
+                )
 
