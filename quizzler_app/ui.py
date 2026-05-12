4619545a1ee839d3e929342f9290bc3c3d015df5
clssadik <cillsadik@gmail.com>
2026-05-12 19:51:54 +0300

    variables

--- quizzler_app/ui.py
+++ quizzler_app/ui.py
@@ -7,7 +7,9 @@ class QuizInterface:
     def __init__(self):
         self.window = Tk()
         self.window.title("Quizzler")
-        
+        self.window.config(bg=THEME_COLOR,width=350,height=500)
+        self.score = 0
+        self.text_lbl = Label(text=f"Score: {score}")
 
 
 
