32ce4233318143c43e18e4a3236a3b289dc0585e
clssadik <cillsadik@gmail.com>
2026-05-12 15:17:36 +0300

    api connection completed

--- quizzler_app/data.py
+++ quizzler_app/data.py
@@ -2,8 +2,7 @@ import requests
 import json
 
 response = requests.get("https://opentdb.com/api.php?amount=10")
-
-
 question_data = response.json()
 
-print(type(question_data))
+with open("data.json","w") as file:
+    json.dump(question_data,file,indent=4)
