be54035e79b1d4a79a1007bd87ccf5da9b41a698
clssadik <cillsadik@gmail.com>
2026-05-14 21:05:41 +0300

    add comment: move to next question

--- quizzler_app/quiz_brain.py
+++ quizzler_app/quiz_brain.py
@@ -15,6 +15,7 @@ class QuizBrain:
     def next_question(self):
         self.current_question = self.question_list[self.question_number]
         self.question_number += 1
+        # Move to the next question in the list
         user_answer = input(f"Q.{self.question_number}: {self.current_question.text} (True/False): ")
         self.check_answer(user_answer)
 
