9f55975d15ad8058002388e813b9f249d46129de
clssadik <cillsadik@gmail.com>
2026-05-14 21:05:31 +0300

    add comment: initialize quiz state

--- quizzler_app/quiz_brain.py
+++ quizzler_app/quiz_brain.py
@@ -6,6 +6,7 @@ class QuizBrain:
         self.question_list = q_list
         self.current_question = None
         # Track current question being displayed
+        # Initialize quiz state with question list
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
