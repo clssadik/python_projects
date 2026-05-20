32a8eddb266d6277f5364bd29e930ea7f30d10f1
clssadik <cillsadik@gmail.com>
2026-05-20 20:04:40 +0300

    current tempature added

--- rain_alert/main.py
+++ rain_alert/main.py
@@ -1,6 +1,8 @@
 import os
 import requests
 from dotenv import load_dotenv
+from twilio.rest import Client
+
 
 load_dotenv()
 
@@ -12,11 +14,14 @@ URL = "https://api.openweathermap.org/data/2.5/forecast"
 params = {
     "lat": LAT,
     "lon": LONG,
-    "appid": API_KEY
+    "appid": API_KEY,
+    "units": "metric"
 }
 
 response = requests.get(URL, params=params)
 data = response.json()
+
+current_temp = data["list"][0]["main"]["temp"]
 
 print(response.status_code)
 
@@ -25,8 +30,18 @@ will_rain = False
 for i in range(0, 5):
     if data["list"][i]["weather"][0]["id"] > 700:
         will_rain = True
-    else:
-        print("Don't you dare :) ")
+    # else:
+    #     print("Don't you dare :) ")
 
 if will_rain:
-    print("Bring an umbrella")
+    account_sid = os.environ["TWILIO_ACCOUNT_SID"]
+    auth_token = os.environ["TWILIO_AUTH_TOKEN"]
+    client = Client(account_sid, auth_token)
+
+    message = client.messages.create(
+        from_='whatsapp:+14155238886',
+        body="yağmur yağıyor mevcut sıcaklık : ",
+        to='whatsapp:+905073519085'
+    )
+
+    print(message.status)
