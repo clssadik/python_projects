b6e9da06eb472f8ca102723f59dce2125b351991
clssadik <cillsadik@gmail.com>
2026-05-31 17:59:13 +0300

    add graph creation

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -5,13 +5,27 @@ load_dotenv()
 
 PIXELA_TOKEN = os.environ["PIXELA_TOKEN"]
 pixela_endpoint = "https://pixe.la/v1/users"
+USERNAME = "clssadik"
 
 user_params = {
     "token": PIXELA_TOKEN,
-    "username": "clssadik",
+    "username": USERNAME,
     "agreeTermsOfService": "yes",
     "notMinor": "yes",
 }
 
-response = requests.post(pixela_endpoint, json=user_params)
-print(response.status_code)
+# response = requests.post(pixela_endpoint, json=user_params)
+# print(response.status_code)
+
+graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
+
+graph_config = {
+    "id" : "graph231242342351",
+    "name" : "Activity Graph",
+    "type" : "commit",
+    "color" : "ichou"
+}
+
+response = requests.post(graph_endpoint,json=graph_config)
+print(response.text)
+
