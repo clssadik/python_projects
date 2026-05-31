67c9da8f037191cd3a4c224c80f4104637c9a743
clssadik <cillsadik@gmail.com>
2026-05-31 18:37:32 +0300

    dynamic date with retry

--- habit_tracker/main.py
+++ habit_tracker/main.py
@@ -1,46 +1,35 @@
 import os
 import requests
+from datetime import date
 from dotenv import load_dotenv
 load_dotenv()
 
 PIXELA_TOKEN = os.environ["PIXELA_TOKEN"]
-pixela_endpoint = "https://pixe.la/v1/users"
 USERNAME = "clssadik2"
-URL_PROGRESS = "https://pixe.la/v1/users/clssadik2/graphs/activity-graph.html"
-
-user_params = {
-    "token": PIXELA_TOKEN,
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes",
-}
-
-# response = requests.post(pixela_endpoint, json=user_params)
-# print(response.text)
-
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
-
-graph_config = {
-    "id": "activity-graph",
-    "name": "Activity Graph",
-    "unit": "commit",
-    "type": "int",
-    "color": "ichou"
-}
+pixela_endpoint = "https://pixe.la/v1/users"
+graph_id = "activity-graph"
 
 headers = {
-    "X-USER-TOKEN" : PIXELA_TOKEN
+    "X-USER-TOKEN": PIXELA_TOKEN
 }
 
-# response = requests.post(graph_endpoint,json=graph_config, headers = headers)
-# print(response.text)
+pixel_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/{graph_id}"
 
-after_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs/activity-graph"
+today = date.today().strftime("%Y%m%d")
 
-after_config = {
-    "date" : "20250531",
-    "quantity" : "5",
+pixel_config = {
+    "date": today,
+    "quantity": "1",
 }
 
-response = requests.post(after_endpoint,json=after_config,headers=headers)
-print(response.text)
+for _ in range(10):
+    response = requests.post(pixel_endpoint, json=pixel_config, headers=headers)
+    data = response.json()
+    if data.get("isSuccess"):
+        print("Success:", today)
+        break
+    elif data.get("isRejected"):
+        print("Rejected, retrying...")
+    else:
+        print(data)
+        break
