7c9204f5f93c13a17ac159222676a1ad283a4369
clssadik <cillsadik@gmail.com>
2026-06-04 15:41:25 +0300

    fix Sheety body format and date types

--- exercise_api/main.py
+++ exercise_api/main.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 import requests
+from datetime import datetime
 
 URL = os.environ["BASE_URL"]
 ID = os.environ["ID"]
@@ -18,6 +19,24 @@ headers_params = {
     "x-app-key": KEY
 }
 
-response = requests.post(URL, headers=headers_params ,json=json_params)
-print(response.status_code)
-print(response.text) 
+response_api = requests.post(URL, headers=headers_params ,json=json_params)
+data_api = response_api.json()
+
+duration = str(data_api["exercises"][0]["duration_min"])
+calories = data_api["exercises"][0]["nf_calories"]
+exercise = str(data_api["exercises"][0]["name"]).capitalize()
+date = str(datetime.now().date())
+time = str(datetime.now().time().strftime("%H:%M:%S"))
+
+body = {
+    "workout": {
+        "date": date,
+        "time": time,
+        "exercise": exercise,
+        "duration": duration,
+        "calories": calories,
+    }
+}
+
+response_sheets = requests.post(SHEETS, json=body)
+print(response_sheets.status_code)
